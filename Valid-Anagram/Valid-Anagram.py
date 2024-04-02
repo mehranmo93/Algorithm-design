209fca72683dcf808967dcf5134283bc4854a4b4
mehran <mehranmohammadi.ai@gmail.com>
2024-04-02 17:00:05 +0200

    up

---
+++ Valid-Anagram/Valid-Anagram.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):  
+            return False
+
+        dict_s = {}
+        dict_t = {}
+
+        for char in s:
+            dict_s[char] = dict_s.get(char,0) + 1
+
+        for char in t:
+            dict_t[char] = dict_t.get(char,0) + 1
+
+        for char, count in dict_s.items():
+            if dict_t.get(char, 0) < count:
+                return False
+                
+        return True
+            
