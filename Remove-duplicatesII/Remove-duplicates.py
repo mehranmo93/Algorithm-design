55f7b72a197a3c3358e7902048e4595770df0372
mehran <mehranmohammadi.ai@gmail.com>
2024-07-28 21:29:38 +0200

    add

---
+++ Remove-duplicatesII/Remove-duplicates.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        write_index = 1
+        count = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+            
+            if count <= 2:
+                nums[write_index] = nums[i]
+                write_index += 1
+        
+        return write_index
